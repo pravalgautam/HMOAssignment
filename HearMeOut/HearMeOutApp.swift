5e5997e041d17e2724892310454ce0ea85cbd023
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2025-03-11 22:55:42 +0530

    Initial Commit

---
+++ HearMeOut/HearMeOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HearMeOutApp.swift
+//  HearMeOut
+//
+//  Created by Praval Gautam on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct HearMeOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
